8445d88ef2fc29fbf27aea687edf9b4eee8cba2c
melkilic <melike180400@gmail.com>
2021-03-14 20:37:10 -0500

    changes completed

--- src/day24inheritanceoverriding/Dog.java
+++ src/day24inheritanceoverriding/Dog.java
@@ -1,7 +1,11 @@
 package day24inheritanceoverriding;
-​public class Dog extends Mammal {
+
+public class Dog extends Mammal {
 	
 	public boolean smellWell;
+	
+	public int height = 25;
+
 	
 	public Dog() {
 		super();//If you type "super()" it is fine, 
@@ -13,7 +17,24 @@ package day24inheritanceoverriding;
 		super(true);
 		//If you do not type any parent constrcutor call inside the first line
 		//Java uses the parent constructor without parameter automatically
+		
+		/*
+		 	"super()" is for "parent constructors call"
+		 	"super" is for "parent variables call"
+		*/
+		
+//		super.haveBaby = true;
+//		super.height=11;
+//		super.weight=22;
+		
+		//super.height; ==> returns the height value from parent classes (Animal)
+		//this.height; ==> return the height value from the class itself (Dog)
+		
+		//If you have multiple variables whose names are same in different parent classes
+		//super keyword selects the closest one
+		
 		this.smellWell = smellWell;
 		
-	}​
+	}
+
 }
